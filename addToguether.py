d90cb0f6ba81a9c191628182bdfccf13cad82af5
Ricardo Saumeth <ricardo.santana@lseg.com>
2024-11-20 16:43:50 +0000

    Fix return type

--- addToguether.py
+++ addToguether.py
@@ -1,7 +1,7 @@
 def add_together(first: float):
     validate_number(first, "First")
 
-    def inner(second: float) -> float:
+    def inner(second: float) -> None:
         validate_number(second, "Second")
         return first + second
 
@@ -10,12 +10,12 @@ def add_together(first: float):
 
 def validate_number(number: float, position: str):
     if not isinstance(number, (int, float)) or isinstance(number, bool):
-        raise ValueError(f"{position} must be  a valid number")
+        raise ValueError(f"{position} must be a valid number")
 
 
 # Example usage
 print(add_together(2)(30))  # Output: 32
 # These will raise ValueError:
-#add_together("not a number")(2)
+# add_together("not a number")(2)
 # add_together(2)("not a number")
-#add_together(True)(2)  # Boolean values are not allowed
+# add_together(True)(2)  # Boolean values are not allowed
