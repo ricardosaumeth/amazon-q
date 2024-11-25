48aa0406da716db82019a96d800024f92defd1df
Ricardo Saumeth <ricardo.santana@lseg.com>
2024-11-25 14:53:44 +0000

    refactor shunk

--- chunk.ts
+++ chunk.ts
@@ -1,4 +1,10 @@
-const chunk = (numbers: number[], size: number): number[][] =>
-  !numbers.length ? [] : [numbers.slice(0, size)].concat(chunk(numbers.slice(size), size))
+const chunk = (numbers: number[], size: number): number[][] => {
+  if (Array.isArray(numbers) && size) {
+    return !numbers.length ? [] : [numbers.slice(0, size)].concat(chunk(numbers.slice(size), size))
+  } else {
+    throw new Error("Numbers must be a valid array of integers and size a valid integer");
+  }
+}
 
-console.log(chunk([1, 2, 3, 4, 5], 2))
+
+console.log(chunk([1, 2, 3, 4, 5], 2)) // [[1,2], [3,4], [5]]
--- chunk_1.py
+++ chunk_1.py
@@ -1,10 +1,10 @@
 def chunk(numbers: list[float], size: int) -> list[list[float]]:
-    if size > 0:
-        raise ValueError("Size must be a positive value")
     if not numbers:
         return []
-    # return [numbers[i:i + size] for i in range(0, len(numbers), size)]
-    return [numbers[:size]] + chunk(numbers[size:], size)
+    if len(numbers) and size:
+        return [numbers[:size]] + chunk(numbers[size:], size)
+    else:
+        raise ValueError("Numbers and size must be valid inputs")
 
 
 print(chunk([1, 2, 3, 4, 5], 2))
